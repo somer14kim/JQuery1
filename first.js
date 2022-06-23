d5ff44631d65123b3de2e13f08b3cd2f3366791d
somer14kim <107061865+somer14kim@users.noreply.github.com>
2022-06-22 21:54:20 -0400

    Add files via upload

---
+++ first.js
@@ -0,0 +1,8 @@
+function alertHover(){
+  document.getElementById("area").style.backgroundColor = "green";
+  alert("Close me to turn the box green.");
+}
+
+function reset(){
+   document.getElementById("area").style.backgroundColor = "#953674"; 
+}
